0cc6f09bc594f8ba2c6ecfeb58ef9926f9529c22
pavel <pavel.polyakov0010@gmail.com>
2024-02-17 20:13:59 +0100

    this shit doesn't work

--- src/components/loadMore/index.jsx
+++ src/components/loadMore/index.jsx
@@ -1,41 +1,69 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 
-export default function LoadMore() {
-    const baseUrl = "https://dummyjson.com/products"
-    const defaultUrl = "https://dummyjson.com/products?limit=10&skip=10&"
+//FIXME: doesn't work
 
+export default function LoadMore() {
     const [products, setProducts] = useState([]);
     const [error, setError] = useState(null);
     const [isLoading, setLoading] = useState(true);
-    const [skip, setSkip] = useState(0);
-    const [url, setUrl] = useState(defaultUrl);
-    const [limit, setLimit] = useState(10);
+    const [count, setCount] = useState(0);
+    const [disableButton, setDisableButton] = useState(false);
 
-    function generateFetchUrl(limit, skip) {
-        const nexSkip = skip + limit;
-
-        setSkip(nexSkip);
-        setUrl(`${baseUrl}?limit=${limit}&skip=${skip}`)
-    }
+    const baseUrl = "https://dummyjson.com/products";
+    const limit = 20;
 
     useEffect(() => {
-        fetch(url)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
+        async function fetchProducts() {
+            try {
+                const response = await fetch(
+                    `${baseUrl}?limit=${limit}&skip=${count === 0
+                        ? 0
+                        : count * limit}`
+                );
+
+                if (!response.ok) {
+                    throw response;
                 }
-                throw response;
-            })
-            .then((data) => setProducts(data.products))
-            .catch((error) => {
-                console.log("Error while fetching data: " + error);
-                setError(error.message);
-            })
-            .finally(() => {
+
+                const data = await response.json();
+
+                setProducts((prevData) => [...prevData, ...data.products]);
+
+                console.log(products);
+                
+            } catch (e) {
+                console.log(e);
                 setLoading(false);
-            })
-    }, [url]);
+                setError(e.massage);
+            }
+        }
+
+        fetchProducts();
+
+        // fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
+        //     .then((response) => {
+        //         if (response.ok) {
+        //             return response.json();
+        //         }
+        //         throw response;
+        //     })
+        //     .then(setProducts((data) => data.products))
+        //     .catch((error) => {
+        //         console.log("Error while fetching data: " + error);
+        //         setError(error.message);
+        //     })
+        //     .finally(() => {
+        //         setLoading(false);
+        //     })
+    }, [count]);
+
+
+    useEffect(() => {
+        if (products && products.length === 100) {
+            setDisableButton(true);
+        }
+    }, [products]);
 
     if (error) {
         return <div>Error: {error}</div>;
@@ -58,17 +86,24 @@ export default function LoadMore() {
                                     className="thumbnail"
                                     src={product.thumbnail}
                                     alt={product.title} />
-                                    <p>{product.title}</p>
+                                <p>{product.title}</p>
                             </div>
                         ))
                         : null
                 }
             </div>
-            
-                <button onClick={() => generateFetchUrl(limit, skip)} className="load-more-button">
+
+            <button
+                onClick={() => setCount(count + 1)}
+                className="load-more-button"
+                disabled={disableButton}>
                 Load More
             </button>
-            
+            {
+                disableButton
+                    ? <p>No more data to load</p>
+                    : null
+            }
         </div>
     )
 }
